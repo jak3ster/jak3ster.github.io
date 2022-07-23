ceb07ced71a884a3df515caa5ce3ad3f3d18c02c
Jay A <jautus@hotmail.com>
2022-07-22 21:20:47 -0700

    Added project#2 and (3)homeworks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,56 @@
 // Portfolio data
 const projects = [
     {
-        project_name: "Price Compare",
+        project_name: "Medical Portal",
         description:
-            "Project 1: Price compare grab the results of searched product using Amazon and Walmart Scrapper API.",
+            `Project 2: A CMS-style user login portal, where patients can secure access 
+            to view personal health information from anywhere with an Internet connection. 
+            Using a secure username and password, patients can view health information 
+            such as: Recent doctor visits. Discharge summaries including chat message.`,
         features:
-            "HTML, CSS, JavaScript, jQuery, Bootstrap, Git branching, Agile Development, Server-side APIs",
-        link: "https://jak3ster.github.io/Price-Search/",
-        github: "https://github.com/jak3ster/Price-Search",
-        image: "assets/images/price-compare-demo.png",
-        image_hover: "assets//demo//price-compare-demo.gif",
-        alt_image: "Screenshot of Price Compare",
-        id: "price_compare",
+            `JavaScript, Node.js, MySQL, npm packages: bcrypt, connect-session-sequelize, dotenv
+            express, express-handlebars, express-session, mysql2, sequelize, moment, socket.io`,
+        link: "https://medicalportalgroup2.herokuapp.com/",
+        github: "https://github.com/jak3ster/Medical-Portal/",
+        image: "assets/images/medical-portal-demo.png",
+        image_hover: "assets/demo/medical-portal-demo.gif",
+        alt_image: "Screenshot of Medical Portal",
+        id: "medical_portal",
+    },
+    {
+        project_name: "My ORM Back End",
+        description: `A back-end for an e-commerce site using a modifying starter code 
+                    and configure a working Express.js API that uses Sequelize to interact 
+                    with a MySQL database.`,
+        features: `JavaScript, Node.js, MySQL, npm packages: express, sequelize, mysql2, dotenv`,
+        link: "https://drive.google.com/file/d/147SxgXKnWxBOA2dynKkdv-qzDa37urxc/view?usp=sharing/",
+        github: "https://github.com/jak3ster/My-ORM-Back-End",
+        image: "assets/images/my-orm-back-end-demo.png",
+        image_hover: "assets/demo/my-orm-back-end-demo.gif",
+        alt_image: "Screenshot of My ORM Back End",
+        id: "my_orm_back_end",
+    },
+    {
+        project_name: "Timed Code Quiz",
+        description: "A timed quiz on HTML/JavaScript that stores high scores.",
+        features: "HTML, CSS, JavaScript, jQuery, Bootstrap",
+        link: "https://jak3ster.github.io/My-Timed-Code-Quiz/",
+        github: "https://github.com/jak3ster/My-Timed-Code-Quiz",
+        image: "assets/images/my-timed-code-quiz-demo.png",
+        image_hover: "assets//demo//my-timed-code-quiz-demo.gif",
+        alt_image: "Screenshot of Code Quiz",
+        id: "code_quiz",
+    },
+    {
+        project_name: "Updated Portfolio #1",
+        description: "This is an updated portfolio as part of building toward being employer-competitive.",
+        features: "HTML, CSS, Bootstrap, JavaScript",
+        link: "https://jak3ster.github.io/My-Updated-Porfolio-Page/",
+        github: "https://github.com/jak3ster/My-Updated-Porfolio-Page/",
+        image: "assets/images/my-updated-portfolio-page.png",
+        image_hover: "assets/demo/my-updated-portfolio-page.gif",
+        alt_image: "Screenshot of Updated Portfolio #1",
+        id: "updated_portfolio",
     },
     {
         project_name: "Password Generator",
@@ -25,15 +64,15 @@ const projects = [
         id: "password_generator",
     },
     {
-        project_name: "Timed Code Quiz",
-        description: "A timed quiz on HTML/JavaScript that stores high scores.",
-        features: "HTML, CSS, JavaScript, jQuery, Bootstrap",
-        link: "https://jak3ster.github.io/My-Timed-Code-Quiz/",
-        github: "https://github.com/jak3ster/My-Timed-Code-Quiz",
-        image: "assets/images/my-timed-code-quiz-demo.png",
-        image_hover: "assets//demo//my-timed-code-quiz-demo.gif",
-        alt_image: "Screenshot of Code Quiz",
-        id: "code_quiz",
+        project_name: "Password Generator",
+        description: "JavaScript Password Generator.",
+        features: "HTML, CSS",
+        link: "https://jak3ster.github.io/My-Password-Generator/",
+        github: "https://github.com/jak3ster/My-Password-Generator",
+        image: "assets/images/my-password-generator-demo.png",
+        image_hover: "assets//demo//my-password-generator-demo.gif",
+        alt_image: "Screenshot of Password Generator",
+        id: "password_generator",
     },
     {
         project_name: "Work Day Scheduler",
